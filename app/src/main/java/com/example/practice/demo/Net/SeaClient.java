d3161efee823c3565d995ce2cfb7f5715455db67
fighter <814130336@qq.com>
2015-07-30 00:20:02 +0800

    成功连接sae 并完成一次收发消息

--- app/src/main/java/com/example/practice/demo/Net/SeaClient.java
+++ app/src/main/java/com/example/practice/demo/Net/SeaClient.java
@@ -1,16 +1,21 @@
 package com.example.practice.demo.Net;
 
 import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.Response;
 import com.ning.http.client.ws.WebSocket;
 import com.ning.http.client.ws.WebSocketTextListener;
 import com.ning.http.client.ws.WebSocketUpgradeHandler;
 import android.util.Log;
+
+import java.util.concurrent.Future;
 
 /**
  * Created by fighter on 15-7-29.
  */
 public class SeaClient extends  Thread {
 
+    private static String wsChannelUrl="http://1.xmsweixin.sinaapp.com/chnl";
+    private String wsAddr="";
 
 
     public SeaClient()
@@ -20,11 +25,25 @@ public class SeaClient extends  Thread {
 
     public void run()
     {
+        try
+        {
+            AsyncHttpClient client=new AsyncHttpClient();
+            Future<Response> f=client.prepareGet(wsChannelUrl).execute();
+            wsAddr=f.get().getResponseBody();
+            Log.d("Demo","web socket address:"+wsAddr);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
         AsyncHttpClient c = new AsyncHttpClient();
+
 
         try {
 
-            WebSocket websocket = c.prepareGet("ws://192.168.1.107:8889")
+            WebSocket websocket = c.prepareGet(wsAddr)
                     .execute(
                             new WebSocketUpgradeHandler.Builder()
                                     .addWebSocketListener(new WebSocketTextListener() {
@@ -32,17 +51,19 @@ public class SeaClient extends  Thread {
                                         @Override
                                         public void onMessage(String message) {
                                             System.out.println(message);
+                                            Log.d("Demo", "recv message"+message);
                                         }
 
 
                                         @Override
                                         public void onOpen(WebSocket websocket) {
+                                            Log.d("Demo","web socket connect succ");
                                             websocket.sendMessage("echo你好啊");
                                         }
 
                                         @Override
                                         public void onClose(WebSocket websocket) {
-                                            Log.d("Demo","web socket Closed");
+                                            Log.d("Demo", "web socket Closed");
                                         }
 
                                         @Override
