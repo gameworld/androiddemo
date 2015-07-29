e3062dcd16d05f57cb9e9617877e8a7964bb0657
caihj <814130336@qq.com>
2015-07-29 17:07:14 +0800

    修改服务

--- app/src/main/java/com/example/practice/demo/service/NetService.java
+++ app/src/main/java/com/example/practice/demo/service/NetService.java
@@ -1,25 +1,27 @@
 package com.example.practice.demo.service;
 
-import android.app.Activity;
-import android.app.IntentService;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.os.IBinder;
-import android.os.ResultReceiver;
 import android.util.Log;
-import com.example.practice.demo.activity.MainActivity;
-import android.support.v4.app.NotificationCompat;
 
+import java.io.InputStream;
+import java.net.Socket;
+import java.nio.ByteBuffer;
 
-import com.example.practice.demo.R;
+import javax.net.SocketFactory;
+
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
 
 public class NetService extends Service {
 
+
+    private NetThread nthread=null;
 
     public NetService()
     {
@@ -34,6 +36,8 @@ public class NetService extends Service {
 
     @Override
     public void onCreate() {
+        nthread=new NetThread("10.2.12.190",9000);
+        nthread.start();
         Log.i("Demo", "service started");
         super.onCreate();
     }
@@ -51,8 +55,115 @@ public class NetService extends Service {
         return START_STICKY;
     }
 
+}
+
+/**
+ * 网络收发线程
+ */
+
+class NetThread extends Thread
+{
+
+    public interface NetCallBack
+    {
+        public void OnData(byte [] data);
+        public void OnClose();
+        public void OnConnect();
+        public void OnError();
+
+    }
+
+    public NetThread(String ip,int port)
+    {
+        this.ip=ip;
+        this.port=port;
+    }
+
+    @Override
+    public void run()
+    {
+        Log.i("Demo","thread started");
+        try
+        {
+            sock= SocketFactory.getDefault().createSocket(ip,port);
+            if(sock.isConnected())
+            {
+                Log.i("Demo", "socket connected");
+                this.callback.OnConnect();
 
 
-    // Send result to activity using ResultReceiver
+                Selector selector=Selector.open();
+                sock.getChannel().register(selector, SelectionKey.OP_READ|SelectionKey.OP_WRITE);
+                sock.getChannel().configureBlocking(false);
+
+                while(true) {
+
+                    int ret = selector.select();
+                    if (ret > 0) {
+
+                        Set<SelectionKey> set=selector.selectedKeys();
+
+                        Iterator<SelectionKey> keyIterator = set.iterator();
+                        while(keyIterator.hasNext()) {
+                            SelectionKey key=keyIterator.next();
+
+                            SocketChannel chnl=(SocketChannel)key.channel();
+                            if(key.isReadable())
+                            {
+                                chnl.read(this.readbuffer);
+                            }
+
+                            if(key.isWritable())
+                            {
+                                chnl.write(this.writebuffer);
+                            }
+
+                            keyIterator.remove();
+                        }
+                    } else {
+                        continue;
+                    }
+
+                }
+
+            }
+            else
+            {
+                this.callback.OnError();
+                Log.i("Demo", "socket connect fail");
+            }
+        }
+        catch (Exception e)
+        {
+            Log.i("Demo","exception :"+e.getMessage());
+            this.callback.OnError();
+        }
+    }
+
+/**
+ */
+    public void Send(ByteBuffer buffer)
+    {
+       // this.writebuffer.put(buffer);
+        //this.sock.getChannel().register();
+
+    }
+
+
+    public void SetCallBack(NetCallBack callback)
+    {
+        this.callback=callback;
+    }
+
+    private Socket sock=null;
+
+
+    private  ByteBuffer writebuffer;
+    private  ByteBuffer readbuffer;
+
+    private String ip;
+    private int port;
+
+    private NetCallBack callback;
 
 }
